539db860e57069aeac0987f03e4bf93e7e7e2685
Mavneesh <minocha999@gmail.com>
2016-10-27 12:36:22 +0530

    UIButton updated

--- Learn/UIButtonViewController.swift
+++ Learn/UIButtonViewController.swift
@@ -10,13 +10,45 @@ import UIKit
 
 class UIButtonViewController: UIKitViewController {
 
+    let myFirstLabel = UILabel();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.title = "UIButton";
+        
+        
+        myFirstLabel.text = "See here while click on press"
+        myFirstLabel.font = UIFont(name: "MarkerFelt-Thin", size: 26)
+        myFirstLabel.textColor = UIColor.redColor()
+        myFirstLabel.textAlignment = .Center
+        myFirstLabel.backgroundColor = UIColor.yellowColor()
+        myFirstLabel.frame = CGRectMake(0,0,300,100)
+        
+        
+        
+        let myFirstButton = UIButton.init(type: UIButtonType.Custom)
+        myFirstButton.setTitle("Press", forState: .Normal)
+        myFirstButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        myFirstButton.tintColor = UIColor.cyanColor()
+        myFirstButton.frame = CGRectMake(0,CGRectGetMaxY(myFirstLabel.frame),300,40)
+        myFirstButton.addTarget(self, action:#selector(UIButtonViewController.pressed(_:)), forControlEvents:.TouchUpInside)
+        
+        
+
+        self.view.addSubview(myFirstLabel)
+        self.view.addSubview(myFirstButton)
+        
+        myFirstLabel.center = CGPointMake(UIScreen.mainScreen().bounds.width/2,200);
+        myFirstButton.center = CGPointMake(UIScreen.mainScreen().bounds.width/2,CGRectGetMaxY(myFirstLabel.frame) + 40);
+        
         // Do any additional setup after loading the view.
     }
 
+    func pressed(button:UIButton) -> Void {
+        
+        myFirstLabel.text =  NSNumber.init(int: rand()).stringValue;
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
